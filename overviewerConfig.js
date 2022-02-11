2eadf5479b2a16ee439efde6d58ff2ea5d8a4da1
Zhe Li <lizhe@lizhe.org>
2022-02-11 19:20:36 +0000

    update

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1644606722",
+        "cacheTag": "1644607236",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
