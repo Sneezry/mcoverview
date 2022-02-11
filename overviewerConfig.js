8204353280fb06efa0182c3e85e868b26b35fc37
Zhe Li <lizhe@lizhe.org>
2022-02-11 13:01:41 +0000

    update

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1644583924",
+        "cacheTag": "1644584500",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1644583819,
+            "last_rendertime": 1644584424,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
